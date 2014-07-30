ccbed0569911e67e717c5a601859977a725081c5
Vinicius <luiz.vinicius73@gmail.com>
2014-07-30 16:29:15 -0300

    new folder structure

---
+++ assets/source/js/_main.js
@@ -0,0 +1,20 @@
+(function () {
+   "use strict";
+   var Projeto;
+
+   $(document).ready(function () {
+      Projeto.init();
+   });
+
+   /**
+    * Funções do projeto
+    * @type {{init: init, run: run}}
+    */
+   Projeto = {
+      init: function () {
+      },
+      run: function () {
+      }
+   };
+
+}).call(this);
