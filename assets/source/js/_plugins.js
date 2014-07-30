13a2f9329c50feddb1b8441d76d26ad58eec77ed
Vinicius <luiz.vinicius73@gmail.com>
2014-07-30 17:00:33 -0300

    fix-2

--- assets/source/js/_plugins.js
+++ assets/source/js/_plugins.js
@@ -1,4 +1,4 @@
-// @koala-prepend "vendor/bootstrap.min.js"
+// @koala-prepend "libs/bootstrap.min.js"
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     for (var a, e = function() {}, b = "assert clear count debug dir dirxml error exception group groupCollapsed groupEnd info log markTimeline profile profileEnd table time timeEnd timeStamp trace warn".split(" "), c = b.length, d = window.console = window.console || {}; c--;) a = b[c], d[a] || (d[a] = e)
