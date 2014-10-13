7eedaf871a603c44e69d8ece31c3c381b41ff696
Vinicius <luiz.vinicius73@gmail.com>
2014-10-13 12:47:34 -0300

    bugfix

--- assets/source/js/_main.js
+++ assets/source/js/_main.js
@@ -1,14 +1,14 @@
-(function($) {
+(function() {
     "use strict";
     var run = function() {
             slider();
         },
         slider = function() {
             // do slider
-        }
+        };
 
     $(document).ready(function() {
         run();
     });
 
-}).call(JQuery);
+}).call(this);
